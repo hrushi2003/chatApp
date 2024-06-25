11ed4f779b3d428774545ccd58a2fb4c83844522
Hrushikesh Vandanapu <94824558+hrushi2003@users.noreply.github.com>
2024-06-25 19:01:08 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,14 +20,8 @@ import { Conversation } from "./models/Conversation.js";
 
 const server = createServer(app);
 const io = new Server(server,{
-     handlePreflightRequest: (req, res) => {
-        const headers = {
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": req.headers.origin,
-            "Access-Control-Allow-Credentials": true
-        };
-        res.writeHead(200, headers);
-        res.end();
+     cors : {
+          origin : "https://chat-app-5g1i.vercel.app"
     },
     connectionStateRecovery: {}
 });
