86d1e84d88a5e3b6819769cb42e9eec527a4be8a
Hrushikesh Vandanapu <94824558+hrushi2003@users.noreply.github.com>
2024-06-25 18:40:57 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,8 +20,14 @@ import { Conversation } from "./models/Conversation.js";
 
 const server = createServer(app);
 const io = new Server(server,{
-    cors : {
-        origin : '*',   
+     handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin,
+            "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers);
+        res.end();
     },
     connectionStateRecovery: {}
 });
