d1c2f0b6a910df2c4fe8ba9682c67608129a50d6
Hrushikesh Vandanapu <94824558+hrushi2003@users.noreply.github.com>
2024-07-07 18:26:29 +0530

    Update AiChat.jsx

--- frontend/src/pages/AiChat.jsx
+++ frontend/src/pages/AiChat.jsx
@@ -11,7 +11,7 @@ const AiChat = () => {
         if(userInp){
           setLoading(true);
           console.log(userInp);
-          await axios.get(`https://chat-app-self-five.vercel.app/chat/ai/${userInp}`).then((response) => {
+          await axios.get(`https://chat-app-lovat-delta.vercel.app/chat/ai/${userInp}`).then((response) => {
             setMessages([...messages,{
               sender : userInp,
               bot: response.data.bot
